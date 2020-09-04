a8c4b226ae0b1c91c287d3b755cd867c188bc365
ayeshaishaq <ayeshaishaq27@gmail.com>
2020-09-04 19:49:54 +0500

    made changes in program

--- program.cpp
+++ program.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 using namespace std;
-
+struct myass {
+	char the;
+	char crack;
+	float WAP;
+};
 int main()
 {
 	cout<<"Hello Mars "<<endl;
-
+	myass ishuge;
+	ishuge.the='A';
+	ishuge.crack='1';
+	ishuge.WAP=10/10;
+	
 	return 0;
 }
